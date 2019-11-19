4f86ca33c142f38a6161c8565007ac1bf2d322f9
Shuya Fuchigami <shuyafuchigami@shmcbkpro17.local>
2019-11-19 11:59:26 +0100

    finish ex2. add files for ex3

--- machine-learning-ex2/ex2/costFunction.m
+++ machine-learning-ex2/ex2/costFunction.m
@@ -7,7 +7,7 @@ function [J, grad] = costFunction(theta, X, y)
 % Initialize some useful values
 m = length(y); % number of training examples
 
-% You need to return the following variables correctly 
+% You need to return the following variables correctly
 J = 0;
 grad = zeros(size(theta));
 
@@ -21,10 +21,13 @@ grad = zeros(size(theta));
 %
 
 
+J = (1/m) * (-y'*log(sigmoid(X*theta)) - (1-y)'*log(1-(sigmoid(X*theta))) );
 
-
-
-
+#結局の所、上記の式を微分するとLinear Regressionのときと同じで
+#下記のようになるようだが、なぜsigmoidはそのままでいいのか？
+# ＝＞ http://forgetzimbnot.com/2014/04/post-211.html
+# sigmoidの微分してもそのまま残るようだ。
+grad = (1/m) * X' * (sigmoid(X*theta)-y);
 
 
 % =============================================================
--- machine-learning-ex2/ex2/costFunctionReg.m
+++ machine-learning-ex2/ex2/costFunctionReg.m
@@ -2,12 +2,15 @@ function [J, grad] = costFunctionReg(theta, X, y, lambda)
 %COSTFUNCTIONREG Compute cost and gradient for logistic regression with regularization
 %   J = COSTFUNCTIONREG(theta, X, y, lambda) computes the cost of using
 %   theta as the parameter for regularized logistic regression and the
-%   gradient of the cost w.r.t. to the parameters. 
+%   gradient of the cost w.r.t. to the parameters.
 
 % Initialize some useful values
 m = length(y); % number of training examples
 
-% You need to return the following variables correctly 
+print("lambda:")
+factor = lambda/(2*m);
+
+% You need to return the following variables correctly
 J = 0;
 grad = zeros(size(theta));
 
@@ -18,10 +21,19 @@ grad = zeros(size(theta));
 %               derivatives of the cost w.r.t. each parameter in theta
 
 
+# do not use the first theta θ0
+J = ((1/m) * (-y'*log(sigmoid(X*theta)) - (1-y)'*log(1-(sigmoid(X*theta))))) + (lambda/(2*m))*sum(theta(2:length(theta)).^2);
 
+#theta_for_gradient = theta;
+#theta_for_gradient(1)  = 0; #do not regularize the first theta θ0.
+#grad = (1/m) * X' * (sigmoid(X*theta)-y) + (lambda/m)*theta_for_gradient;
 
-
+#TODO: fix
+#' better implementation.
+#grad = theta*(1-lambda/m) - (1/m) * X' * (sigmoid(X*theta)-y);
 
 % =============================================================
+
+
 
 end

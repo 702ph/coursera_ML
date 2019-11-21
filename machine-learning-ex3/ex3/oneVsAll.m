0b8ed96780a48d6bef444660cce8d70cc1ae1949
Shuya Fuchigami <shuyafuchigami@shmcbkpro17.local>
2019-11-21 18:10:51 +0100

    begin ex3

--- machine-learning-ex3/ex3/oneVsAll.m
+++ machine-learning-ex3/ex3/oneVsAll.m
@@ -1,17 +1,19 @@
 function [all_theta] = oneVsAll(X, y, num_labels, lambda)
 %ONEVSALL trains multiple logistic regression classifiers and returns all
-%the classifiers in a matrix all_theta, where the i-th row of all_theta 
+%the classifiers in a matrix all_theta, where the i-th row of all_theta
 %corresponds to the classifier for label i
 %   [all_theta] = ONEVSALL(X, y, num_labels, lambda) trains num_labels
 %   logistic regression classifiers and returns each of these classifiers
-%   in a matrix all_theta, where the i-th row of all_theta corresponds 
+%   in a matrix all_theta, where the i-th row of all_theta corresponds
 %   to the classifier for label i
 
-% Some useful variables
-m = size(X, 1);
-n = size(X, 2);
+# num_labesはクラスの数が入っている。ここでは 10。
 
-% You need to return the following variables correctly 
+% Some useful variables
+m = size(X, 1);  # 5000 training examples トレーニングデータ
+n = size(X, 2);  # 400 = 20x20 のピクセルデータ。400のfeaturesである。
+
+% You need to return the following variables correctly
 all_theta = zeros(num_labels, n + 1);
 
 % Add ones to the X data matrix
@@ -20,7 +22,9 @@ X = [ones(m, 1) X];
 % ====================== YOUR CODE HERE ======================
 % Instructions: You should complete the following code to train num_labels
 %               logistic regression classifiers with regularization
-%               parameter lambda. 
+%               parameter lambda.
+# 和訳：コードを完成せて num_labels個のLogistic Regression 分類機を完成させよ。その際、regularizationのパラメータにはlambdaを用いよ。
+
 %
 % Hint: theta(:) will return a column vector.
 %
@@ -38,27 +42,34 @@ X = [ones(m, 1) X];
 %
 %     % Set Initial theta
 %     initial_theta = zeros(n + 1, 1);
-%     
+%
 %     % Set options for fminunc
 %     options = optimset('GradObj', 'on', 'MaxIter', 50);
-% 
+%
 %     % Run fmincg to obtain the optimal theta
-%     % This function will return theta and the cost 
+%     % This function will return theta and the cost
 %     [theta] = ...
 %         fmincg (@(t)(lrCostFunction(t, X, (y == c), lambda)), ...
 %                 initial_theta, options);
 %
 
 
+for label = 1:num_labels
+  theta = ones(n+1,1);
+  y_binarized = (y==label);
 
+  cost = ((1/m) * (-y_binarized'*log(sigmoid(X * theta))- (1-y_binarized)'*log(1-sigmoid((X*theta)))));
+  regularization = (lambda/(2*m))*sum(theta(2:end,:).^2);
+  J = cost + regularization;
 
+  theta_for_grad = [0; theta(2:end,:)];
+  grad =  (lambda/m)*theta_for_grad + (1/m) * X' * (sigmoid(X*theta)-y);
 
+  all_theta(label,:) = (theta - grad)';
+end
 
-
-
-
-
-
+disp("debug")
+all_theta
 
 % =========================================================================
 
